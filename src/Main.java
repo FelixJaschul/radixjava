245867d0977e3b66be913f4cc0520477a3c64c5b
felixjaschul <Felix@Jaschul.de>
2025-04-14 21:56:28 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,44 @@ import java.util.Random;
 
 class Main {
 
-    private static void countingSort(int[] array, int exp) {
-        int n = array.length;
+    private static void countingSort(int[] data, int exp) {
+        int n = data.length;
         int[] output = new int[n];
         int[] count = new int[10];
 
         for (int i = 0; i < n; ++i)
-            count[(array[i] / exp) % 10]++;
+            count[(data[i] / exp) % 10]++;
 
         for (int i = 1; i < 10; ++i)
             count[i] += count[i - 1];
 
         for (int i = n - 1; i >= 0; --i) {
-            output[count[(array[i] / exp) % 10] - 1] = array[i];
-            --count[(array[i] / exp) % 10];
+            output[count[(data[i] / exp) % 10] - 1] = data[i];
+            --count[(data[i] / exp) % 10];
         }
 
-        System.arraycopy(output, 0, array, 0, n);
+        System.arraycopy(output, 0, data, 0, n);
     }
 
-    public static void sort(int[] array, int n) {
-        int maxElement = Arrays.stream(array).max().getAsInt();
+    public static void sort(int[] data, int n) {
+        int maxElement = Arrays.stream(data).max().getAsInt();
         for (int i = 0; i < n; i++)
             for (int exp = 1; maxElement / exp > 0; exp *= 10)
-                countingSort(array, exp);
+                countingSort(data, exp);
     }
 
     public static void main(String[] args) {
         int n = (int)1e2;
         int size = (int)1e6;
-        int[] randomArr = new int[size];
+        int[] data = new int[size];
 
         // Generate a random array
         for (int i = 0; i < size; ++i)
-            randomArr[i] = new Random().nextInt(999);
+            data[i] = new Random().nextInt(999);
 
         // Sort the array using radix sort
         long start = System.nanoTime();
-        sort(randomArr, n);
+        sort(data, n);
         long end = System.nanoTime();
 
         double ms = ((end - start) / 1e6) / n; // to milliseconds
